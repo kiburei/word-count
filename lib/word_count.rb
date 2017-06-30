e143886695857f24529214c41739b365472c35d5
kiburei <akiburei@gmail.com>
2017-06-30 11:32:53 +0300

    Final spec to check how many times a word is repeated passes

--- lib/word_count.rb
+++ lib/word_count.rb
@@ -1,6 +1,13 @@
 class String
   define_method(:word_count) do
-    word = "one"
-    self.include? word
+    compare = "one"
+    count = 0
+    sentence = self.split()
+    sentence.each() do |word|
+      if word == compare
+        count += 1
+      end
+    end
+    count
   end
 end
--- spec/word_count_spec.rb
+++ spec/word_count_spec.rb
@@ -7,11 +7,11 @@ require ('rspec')
       expect("one".word_count()).to(eq(true))
     end
 
-    it("check if string present in sentence") do
+    xit("check if string present in sentence") do
       expect("ruby specs one O one".word_count()).to(eq(true))
     end
 
-    xit("returns number of times a word appears in a sentence") do
+    it("returns number of times a word appears in a sentence") do
       expect("ruby specs one O one".word_count()).to(eq(2))
     end
 
